cf7bd29a4470ff292c38d0525139950cccb4bf6d
loloof64 <laurent.bernabe@gmail.com>
2022-11-12 12:01:44 +0100

    setting minimal window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:window_size/window_size.dart';
 
 import 'screens/new_gif_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Chess animated gif creator');
+    setWindowMinSize(const Size(400, 300));
+    setWindowMaxSize(Size.infinite);
+  }
+
   runApp(const MyApp());
 }
 
